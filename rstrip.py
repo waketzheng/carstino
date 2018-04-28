f7e34b736c0d09f22ac35a2e23633c8c4a4dbaae
waketzheng <waketzheng@gmail.com>
2018-04-28 16:07:14 +0800

    add -r argument for rstrip.py

--- rstrip.py
+++ rstrip.py
@@ -19,21 +19,42 @@ def rstrip_file(fname, newlines=1):
 
 
 def main():
+    import re
+    import os
     import sys
+    from argparse import ArgumentParser
     if not sys.argv[1:]:
         print(__doc__.strip())
         print('\nUsage:')
         print('{}{} /path/to/file'.format(' '*4, sys.argv[0]))
         return
-    files = sys.argv[1:]
+    parser = ArgumentParser()
+    parser.add_argument('-R', '-r', action='store_true',
+                        help='whether to resurive')
+    args = parser.parse_args()
+    if args.R:
+        d = '.'
+        files = []
+        pattern_hiden = re.compile(r'\.\w')
+        for r, ds, fs in os.walk(d):
+            if any(pattern_hiden.match(i) for i in r.split(os.path.sep)):
+                continue
+            for fn in fs:
+                if not pattern_hiden.match(fn):
+                    files.append(os.path.join(r, fn))
+    else:
+        files = sys.argv[1:]
+    count_skip = count_rstrip = 0
     for fn in files:
         try:
             rstrip_file(fn)
         except Exception as e:
+            count_skip += 1
             print('{}: skip! {}'.format(fn, e))
         else:
+            count_rstrip += 1
             print('{}: rstriped.'.format(fn))
-    print('Done!')
+    print('Done! {} skiped, {} rstriped.'.format(count_skip, count_rstrip))
 
 
 if __name__ == '__main__':
