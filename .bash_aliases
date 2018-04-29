2e8d73c6d6e9fb7dddf2fe9ec369650aabfd564c
waketzheng <waketzheng@gmail.com>
2018-04-29 13:14:45 +0800

    use py3.6 to switch pip source

--- .bash_aliases
+++ .bash_aliases
@@ -3,8 +3,8 @@ alias mg='python manage.py'
 
 # python pipenv
 alias ve="pipenv shell"
-alias env="pipenv --three&&python3 ~/.switch_source_pipenv.py&&ve"
-alias env2="pipenv --two&&python3 ~/.switch_source_pipenv.py&&ve"
+alias env="pipenv --three&&python3.6 ~/.switch_source_pipenv.py&&ve"
+alias env2="pipenv --two&&python3.6 ~/.switch_source_pipenv.py&&ve"
 alias pgg="pipenv graph"
 alias pii="pipenv install"
 alias poo="pipenv open "
--- .switch_source_pipenv.py
+++ .switch_source_pipenv.py
@@ -1,14 +1,35 @@
-#!/usr/bin/env python
-import os
+#!/usr/bin/env python3.6
+from pathlib import Path
 
 
 fname = 'Pipfile'
 
-if os.path.exists(fname):
-    with open(fname, 'r+') as f:
-        s = f.read()
-        f.seek(0)
-        f.write(s.replace(
-            'pypi.python.org',
-            'mirrors.aliyun.com/pypi'
-        ))
+aliyun = '''
+[[source]]
+url = "https://mirrors.aliyun.com/pypi/simple"
+verify_ssl = true
+name = "aliyun"
+
+'''
+
+douban = '''
+[[source]]
+url = "https://pypi.douban.com/simple"
+verify_ssl = true
+name = "douban"
+
+'''
+
+def main():
+    import sys
+    p = Path(fname)
+    if p.exists():
+        source = douban if 'douban' in sys.argv else aliyun
+        with p.open('r+') as f:
+            s = f.read()
+            f.seek(0)
+            f.truncate()
+            f.write(source.lstrip()+s)
+
+if __name__ == '__main__':
+    main()
