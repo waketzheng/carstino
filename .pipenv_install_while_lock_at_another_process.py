ddd2d3c37c0c44632fa10cdc9f2d11f4ab1089b2
waketzheng <waketzheng@gmail.com>
2018-05-10 07:45:06 +0000

    so fast so good

--- .pipenv_install_while_lock_at_another_process.py
+++ .pipenv_install_while_lock_at_another_process.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3.6
 import os
 import sys
-from pathlib import Path
-from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Process
 
 
@@ -13,15 +11,9 @@ def subcmd(cmd):
 
 def main():
     os.system(f'pipenv install --skip-lock {" ".join(sys.argv[1:])}')
-    # p = Process(target=subcmd, args=('pipenv lock &>/dev/null',))
-    # p.start()
-    # executor = ProcessPoolExecutor()
-    # yield executor.submit(subcmd, 'pipenv lock>/dev/null')
-    # executor.submit(subcmd, 'pipenv lock>/dev/null')
-    ProcessPoolExecutor().submit(subcmd, 'pipenv lock')
+    p = Process(target=subcmd, args=("pipenv lock 2>&1 >/dev/null",))
+    p.start()
 
 
-
-if __name__ == '__main__':
-    a = main()
-    # next(a)
+if __name__ == "__main__":
+    main()
