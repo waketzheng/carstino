46bcf48eec2d2cb2e186e3d26b488e70888f80a1
waketzheng <waketzheng@gmail.com>
2018-05-11 19:56:50 +0800

    use py3.6 and pathlib for pip_conf.py

--- pip_conf.py
+++ pip_conf.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 import os
+from pathlib import Path
 
 
 aliyun = '''
@@ -17,46 +18,36 @@ trusted-host = pypi.douban.com
 '''
 
 
-def pip_conf_path(user=None):
-    user = user or os.popen('whoami').read().strip()
-    return '/home/{}/.pip/pip.conf'.format(user)
-
-
-def init_pip_conf(source=aliyun, conf_path=None, user=None):
-    fname = conf_path or pip_conf_path(user)
-    dn = os.path.dirname(fname)
-    if not os.path.exists(dn):
-        os.system('mkdir {}'.format(dn))
-    with open(fname, 'w') as f:
-        f.write(source.lstrip())
+def init_pip_conf(source=aliyun, replace=False):
+    p = Path.home() / '.pip/pip.conf'
+    if not p.parent.exists():
+        p.parent.mkdir()
+    source = source.strip()
+    if p.exists():
+        if source in p.read_text():
+            return
+        if not replace:
+            print('pip.conf exists! content:')
+            print(p.read_text())
+            print('If you want to replace it,rerun this script and add "-y" in args')
+            return
+    p.write_text(source+'\n')
+    print(f'Write lines to `{p}` as below:')
+    print(source)
 
 
 def main():
     import sys
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument(
-        '-u', '--user',
-        help='>> /home/{user}/.pip/pip.conf [default:`whoami`]')
     parser.add_argument('-y', action='store_true',
                         help='whether replace exist file')
     parser.add_argument('-s', '--source', default='aliyun',
                         help='the source of pip, douban or aliyun(default)')
     args = parser.parse_args()
-    user = args.user
-    fname = pip_conf_path(user)
-    if os.path.exists(fname) and '-y' not in sys.argv:
-        print('pip.conf exists!')
-        print('If you want to replace it, add "-y" in args')
-        return
-    if sys.argv[1:] and 'douban' in sys.argv:
-        source = douban
-    else:
-        source = aliyun
-    init_pip_conf(source, fname)
+    source = douban if args.source == 'douban' else aliyun
+    init_pip_conf(source, args.y)
     print('Done!')
-    print('Write lines to `{}` as below:'.format(fname))
-    print(source)
 
 
 if __name__ == '__main__':
