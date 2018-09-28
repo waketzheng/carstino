ba3c4b36215977bed3fdb9e5a4ac90cde165be94
waketzheng <waketzheng@gmail.com>
2018-09-28 22:36:23 +0800

    activate git completion when activate mg completion

--- init_my_dev.py
+++ init_my_dev.py
@@ -22,9 +22,9 @@ PACKAGES = "pipenv ipython django pylint flake8 white black"
 
 def main():
     home = Path.home()
-    aliases = home / ALIAS_FILE
-    if aliases.exists():
-        a = input(f"`{aliases}` exists. Continue and replace it?[y/(n)] ")
+    aliases_path = home / ALIAS_FILE
+    if aliases_path.exists():
+        a = input(f"`{aliases_path}` exists. Continue and replace it?[y/(n)] ")
         if not a.lower().strip().startswith("y"):
             return
     try:
@@ -33,19 +33,13 @@ def main():
         repo = Path(".").resolve()
     for fn in FILES:
         os.system(f"cp {repo / fn} {home}")
-    s = aliases.read_text()
+    s = aliases_path.read_text()
     ss = re.sub(r'(rstrip=")(.*)"', rf'\1{repo/"rstrip.py"}"', s)
     with os.popen("alias") as fp:
         if "alias vi=" not in fp.read():
             ss += "alias vi=vim\n"
     if s != ss:
-        aliases.write_text(ss)
-    # switch pip source to aliyun
-    swith_pip_source = repo / "pip_conf.py"
-    os.system(f"{swith_pip_source}")
-    os.system("sudo cp -r ~/.pip /home/root/")
-    # git push auto fill in username and password after input once
-    os.system("git config --global credential.helper store")
+        aliases_path.write_text(ss)
     # auto complete for command `mg`
     mg_completion_path = home / ".mg_completion.bash"
     if not mg_completion_path.exists():
@@ -56,29 +50,37 @@ def main():
                 os.system(f"sudo cp {repo/fname} {sys_completion_path}")
         else:
             os.system(f"cp {repo/fname} {mg_completion_path}")
-    # Install some useful python modules
-    if os.system(f"python3 -m pip install --upgrade --user {PACKAGES}") != 0:
-        if os.system(f"sudo pip3 install -U {PACKAGES}") != 0:
-            print("Please install python3-pip and then rerun this script.")
-            return
+    # append mg and git completion activate alias to aliases file
+    if mg_completion_path.exists():
+        a = f"source {mg_completion_path}"
+    else:
+        a = f"source {sys_completion_path/fname}"
+    git_completion_path = Path("/etc/bash_completion.d/git-completion.bash")
+    if git_completion_path.exists():
+        a += f"&&source {git_completion_path}"
+    if a not in aliases_path.read_text():
+        os.system(f"echo 'alias activate_completion=\"{a}\"'>>{aliases_path}")
     # activate aliases at .bashrc
     bashrc = home / ".bashrc"
     if ALIAS_FILE not in bashrc.read_text():
         with bashrc.open("a") as fp:
             fp.write(f"[[ -f ~/{ALIAS_FILE} ]] && . ~/{ALIAS_FILE}")
-    # append mg completion activate alias to aliases file
-    if mg_completion_path.exists():
-        a = f"source {mg_completion_path}"
-    else:
-        a = f"source {sys_completion_path/fname}"
-    if a not in home.joinpath(ALIAS_FILE).read_text():
-        os.system(f"echo 'alias activate_mg_completion=\"{a}\"'>>{aliases}")
-
+    # switch pip source to aliyun
+    swith_pip_source = repo / "pip_conf.py"
+    os.system(f"{swith_pip_source}")
+    os.system("sudo cp -r ~/.pip /home/root/")
+    # git push auto fill in username and password after input once
+    os.system("git config --global credential.helper store")
+    # Install some useful python modules
+    if os.system(f"python3 -m pip install --upgrade --user {PACKAGES}") != 0:
+        if os.system(f"sudo pip3 install -U {PACKAGES}") != 0:
+            print("Please install python3-pip and then rerun this script.")
+            return
     # make sure pipenv work
     if os.system("pipenv --version") != 0:
         with os.popen("which python3") as p:
             cmd = f"{p.read()} -m pipenv"
-        os.system(f"echo 'alias pipenv=\"{cmd}\"'>>{aliases}")
+        os.system(f"echo 'alias pipenv=\"{cmd}\"'>>{aliases_path}")
     # add pipenv auto complete to user profile
     a = 'eval "$(pipenv --completion)"'
     ps = home.glob(".*profile")
