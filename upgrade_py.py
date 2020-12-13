2edf31074b95aa7e13584a03baf90b9b5ed5cd2a
waketzheng <waketzheng@gmail.com>
2020-12-13 23:28:23 +0800

    fixing upgrade py

--- upgrade_py.py
+++ upgrade_py.py
@@ -11,9 +11,15 @@ This script do the following steps:
         ./configure --enable-optimizations --enable-loadable-sqlite-extensions\
                 && make && sudo make altinstall
 """
+import argparse
 import os
 import sys
 import time
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 VERSION = "3.9.1"
 DOWNLOAD_URL = "https://mirrors.huaweicloud.com/python/{0}/Python-{0}.tar.xz"
@@ -23,6 +29,13 @@ INSTALL = (
     " && make && sudo make {}install"
 )
 DEFAULT_DIR = "~/softwares"
+
+# Only for ubuntu
+EXTEND = (
+    "build-essential libssl-dev bzip2 libbz2-dev libxml2-dev "
+    "libxslt1-dev zlib1g-dev libffi-dev"
+)
+APPEND = "sudo apt install -y python3-dev"
 
 # To install python2, just run `sudo apt install python2`
 SHORTCUTS = {
@@ -49,61 +62,88 @@ def run_and_echo(cmd):
     return os.system(cmd)
 
 
+def parse_args():
+    """parse custom arguments and set default value"""
+    parser = argparse.ArgumentParser(description="Install python by source")
+    parser.add_argument(
+        "-d", "--dir", nargs="?", default=DEFAULT_DIR, help="Where to install"
+    )
+    parser.add_argument(
+        "-v", "--version", nargs="?", default=VERSION, help="Python version"
+    )
+    parser.add_argument(
+        "--dep", action="store_true", help="Install some ubuntu packages"
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Force update (example:3.8.1 -> 3.8.6)",
+    )
+    parser.add_argument("--alt", action="store_true", help="Want altinstall?")
+    return parser.parse_args()
+
+
+def validated_args():
+    args = parse_args()
+    # if '.' in args.version:
+    #     assert args.version >= '3', 'Only support Python3 install'
+    # else:
+    #     assert args.version in SHORTCUTS, 'Version should be in ' + str(list(SHORTCUTS.keys()))
+    if "." in args.v:
+        assert args.v >= "3", "Only support Python3 install"
+    elif args.v not in SHORTCUTS:
+        raise AssertionError("Version should be in " + repr(list(SHORTCUTS.keys())))
+    return args
+
+
 def main():
+    args = validated_args()
     # is_ubuntu = bool(silently_run("which apt"))
+    version = args.version
     start = time.time()
-    sys_argv = sys.argv[1:]
-    for idx, arg in enumerate(sys_argv):
-        # todo: use argparser instead
-        if arg == "-v":
-            v = sys_argv[idx + 1]
-            version = SHORTCUTS.get(v, v)
-            break
-        if arg.startswith("--version="):
-            v = arg.replace("--version=", "")
-            version = SHORTCUTS.get(v, v)
-            break
-    else:
-        version = VERSION
     target_version = version.rsplit(".", 1)[0]
-    force_upgrade = "-f" in sys_argv or "--force" in sys_argv
-    py_version = default_python_version()
-    if not force_upgrade:
+    current_version = default_python_version()
+    if not args.force_upgrade:
         pyx = "python" + target_version
         tip = "\n{} already installed. ".format(pyx.title())
-        if py_version.startswith(target_version):
+        if current_version.startswith(target_version):
             run_and_echo("python -V")
-            if py_version < version:
+            if current_version < version:
                 tip += "Do your want to upgrade to {}? [y/N] ".format(version)
             else:
                 tip += "Do your want to reinstall? [y/N] "
             a = input(tip)
             if a.strip().lower() != "y":
-                return
+                sys.exit()
         elif silently_run("which {}".format(pyx)).strip():
             run_and_echo("{} -V".format(pyx))
             a = input(tip + "Do your want to reinstall? [y/N] ")
             if a.strip().lower() != "y":
-                return
+                sys.exit()
     url = DOWNLOAD_URL.format(version)
     # if system python is py3, replace it, else `make altinstall`
-    alt = not py_version.startswith("3") or "--alt" in sys_argv
-    folder = DEFAULT_DIR
-    for idx, arg in enumerate(sys_argv):
-        # todo: use argparser instead
-        if arg == "-d":
-            folder = sys_argv[idx + 1]
-            break
-        if arg.startswith("--dir="):
-            folder = arg.replace("--dir=", "")
-            break
+    alt = args.alt or not current_version.startswith("3")
+    folder = args.dir
     install_py(folder, url, alt)
-    cost = int(time.time() - start)
+    print("Done! Cost:", friendly_time(int(time.time() - start)))
+
+
+def friendly_time(cost):
     if cost >= 3600:
-        cost = ">= {}h".format(cost // 3600)
+        return ">= {}h".format(cost // 3600)
     elif cost > 60:
-        cost = "{}m{}s".format(cost // 60, cost % 60)
-    print("Done! Cost:", cost)
+        return "{}m{}s".format(cost // 60, cost % 60)
+    return "{} seconds".format(cost)
+
+
+def home():
+    try:
+        return os.environ["HOME"]
+    except KeyError:
+        import pwd
+
+        return pwd.getpwuid(os.getuid()).pw_dir
 
 
 def install_py(folder, url, alt):
