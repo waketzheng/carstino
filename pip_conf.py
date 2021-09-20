f086f7986a9a5e9a6cd90fe0c1fdcd59bf85b935
wj <zhengwenjie@esoaru.com>
2021-09-21 01:42:05 +0800

    Set pip default to tx and auto detect cloud server case

--- pip_conf.py
+++ pip_conf.py
@@ -4,23 +4,28 @@ This script is for pip source config.
 Worked both windows and linux, support python2.7 and python3.5+
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Use:
-    $ python pip_conf.py
+    $ python pip_conf.py  # default to tencent source
+
+Or:
     $ python pip_conf.py -s tx
     $ python pip_conf.py -s aliyun
     $ python pip_conf.py -s douban
     $ python pip_conf.py -s qinghua
+    $ sudo python pip_conf.py -r  # Set conf file to /etc
 """
 import os
 import platform
 import re
+import socket
 
 """
 A sample of the pip.conf/pip.ini:
 ```
 [global]
-index-url = https://mirrors.aliyun.com/pypi/simple/
+index-url = https://mirrors.cloud.tencent.com/pypi/simple/
+
 [install]
-trusted-host = mirrors.aliyun.com
+trusted-host = mirrors.cloud.tencent.com
 ```
 """
 
@@ -30,34 +35,69 @@ index-url = https://{}/simple/
 [install]
 trusted-host = {}
 """.strip()
-DEFAULT = "aliyun"
+DEFAULT = "tx"
 SOURCES = {
     "aliyun": "mirrors.aliyun.com/pypi",
     "douban": "pypi.douban.com",
     "qinghua": "pypi.tuna.tsinghua.edu.cn",
     "tx": "mirrors.cloud.tencent.com/pypi",
+    "tx_ecs": "mirrors.tencentyun.com/pypi",
     "huawei": "repo.huaweicloud.com/repository/pypi",
+    "ali_ecs": "mirrors.cloud.aliyuncs.com/pypi",
 }
 SOURCES["tencent"] = SOURCES["tengxun"] = SOURCES["tx"]
+SOURCES["ali"] = SOURCES["aliyun"]
 conf_cmd = "pip config set global.index-url https://{}/simple/"
 
 
-def init_pip_conf(source=DEFAULT, replace=False):
+def is_pingable(domain):
+    try:
+        socket.gethostbyname(domain)
+    except Exception:
+        return False
+    return True
+
+
+def is_tx_cloud_server():
+    return is_pingable("mirrors.tencentyun.com")
+
+
+def is_ali_cloud_server():
+    return is_pingable("mirrors.cloud.aliyuncs.com")
+
+
+def init_pip_conf(
+    source=DEFAULT, replace=False, root=False, force=False, template=TEMPLATE
+):
+    if not force:
+        if "ali" in source:
+            if is_ali_cloud_server():
+                source = "ali_ecs"
+                template = template.replace("https", "http")
+        elif "tx" in source or "ten" in source:
+            if is_tx_cloud_server():
+                source = "tx_ecs"
+                template = template.replace("https", "http")
     url = SOURCES.get(source, SOURCES[DEFAULT])
-    if can_set_global():
+    is_windows = platform.system() == "Windows"
+    if (not root or is_windows) and can_set_global():
         cmd = conf_cmd.format(url)
         print("--> " + cmd)
         os.system(cmd)
         return
-    if platform.system() == "Windows":
+    if is_windows:
         _pip_conf = ("pip", "pip.ini")
+        conf_file = os.path.join(os.path.expanduser("~"), *_pip_conf)
     else:
-        _pip_conf = (".pip", "pip.conf")
-    conf_file = os.path.join(os.path.expanduser("~"), *_pip_conf)
+        if root:
+            conf_file = os.path.join("/etc", "pip.conf")
+        else:
+            _pip_conf = (".pip", "pip.conf")
+            conf_file = os.path.join(os.path.expanduser("~"), *_pip_conf)
     parent = os.path.dirname(conf_file)
     if not os.path.exists(parent):
         os.mkdir(parent)
-    text = TEMPLATE.format(url, url.split("/")[0])
+    text = template.format(url, url.split("/")[0])
     if os.path.exists(conf_file):
         with open(conf_file) as fp:
             s = fp.read()
@@ -89,15 +129,16 @@ def main():
 
     parser = ArgumentParser()
     parser.add_argument("-y", action="store_true", help="whether replace existing file")
+    parser.add_argument("-r", action="store_true", help="Set conf file to /etc")
+    parser.add_argument("-f", action="store_true", help="Force to skip ecs cloud check")
     parser.add_argument(
         "-s",
         "--source",
         default=DEFAULT,
-        help="the source of pip, douban/qinghua or aliyun(default)",
+        help="the source of pip, douban/qinghua/aliyun or tx(default)",
     )
     args = parser.parse_args()
-    source = args.source
-    init_pip_conf(source, args.y)
+    init_pip_conf(args.source, args.y, args.r, args.f)
 
 
 if __name__ == "__main__":
