536d9454a64088cc659fbacdd9a4c7daafdabb39
wj <zhengwenjie@esoaru.com>
2022-01-26 23:26:48 +0800

    Avoid reinstall for frontend conf script

--- frontend_conf.sh
+++ frontend_conf.sh
@@ -1,24 +1,32 @@
 # ---- For frontend development
-echo "---- nodejs/npm/yarn/vue for frontend"
-export NPM_MIRROR=http://mirrors.cloud.tencent.com/npm/
-sudo apt install -y nodejs
-sudo apt install -y npm
-sudo npm i -g npm --registry $NPM_MIRROR
-sudo npm i -g yarn --registry $NPM_MIRROR
+echo "---- nodejs/npm/yarn/zx for frontend"
+if [[ -n $1 ]]; then
+  export NPM_MIRROR="$1"
+else
+  export NPM_MIRROR=http://mirrors.cloud.tencent.com/npm/
+fi
+which node || sudo apt install -y nodejs
+which npm || (sudo apt install -y npm && sudo npm i -g npm --registry $NPM_MIRROR)
+which yarn || sudo npm i -g yarn --registry $NPM_MIRROR
 yarn config set registry $NPM_MIRROR -g
 # https://github.com/google/zx.git
-yarn global add zx
+if ! command -v zx  &> /dev/null; then
+  echo '`zx` not found! Start to install it by `yarn global add`'
+  yarn global add zx
+fi
 
 # to be optimize: Use `zx` to make nvm installation as optional
-echo "---- Optional install nvm to manage nodejs version"
-export NVM_NODEJS_ORG_MIRROR=https://repo.huaweicloud.com/nodejs/
-git clone https://gitee.com/mirrors/nvm.git ~/.nvm && \
-  cd ~/.nvm && \
-  git checkout `git describe --abbrev=0 --tags` && \
-  ./install.sh && \
-  source ./nvm.sh && \
-  cd - && \
-  nvm install --lts && \
-  npm config set registry $NPM_MIRROR
-# ---- End For
-
+if ! command -v nvm  &> /dev/null; then
+  echo '`nvm` already installed. Skip!'
+else
+  echo "---- Optional install nvm to manage nodejs version"
+  export NVM_NODEJS_ORG_MIRROR=https://repo.huaweicloud.com/nodejs/
+  which nvm || git clone https://gitee.com/mirrors/nvm.git ~/.nvm && \
+    cd ~/.nvm && \
+    git checkout `git describe --abbrev=0 --tags` && \
+    ./install.sh && \
+    source ./nvm.sh && \
+    cd - && \
+    nvm install --lts && \
+    npm config set registry $NPM_MIRROR
+fi
