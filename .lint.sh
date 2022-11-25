6e9602516f6f6d6d330347f49cf128bd5357cc80
waketzheng <waketzheng@gmail.com>
2022-11-25 23:46:44 +0800

    Auto change flake8 to pflake8 when it exists

--- .lint.sh
+++ .lint.sh
@@ -1,3 +1,7 @@
-isort $* && black $* && flake8 $* && mypy --ignore-missing-imports $* && \
-echo 'Run `'"isort $* && black $* && flake8 $* && mypy --ignore-missing-imports $*"'`' && \
-echo Done.
+#!/bin/bash
+FLAKE8=`(which pflake8 >> /dev/null) && (echo pflake8) || (echo flake8)`
+CMD="isort $* && black $* && $FLAKE8 $* && mypy --ignore-missing-imports $*"
+
+echo 'Run `'$CMD'`' && \
+sh -c "$CMD" && \
+  echo Done.
