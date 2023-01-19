d0c891a8e152314361605865a84cbb6690adfd2e
Waket Zheng <waketzheng@gmail.com>
2023-01-20 01:54:11 +0800

    Add script for cloud server ssh/scp

---
+++ to_my_server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#IP_OR_DOMAIN=www.mydomain.com
+IP_OR_DOMAIN=43.139.125.122
+USER=ubuntu
+#PORT=22
+#PRI=~/.ssh/id_rsa
+
+HOST=$USER@$IP_OR_DOMAIN
+if [ $1 ]; then
+  cmd="scp -r"
+  if [ $PRI ]; then
+    cmd="$cmd -i $PRI"
+  fi
+  if [ $PORT ]; then
+    cmd="$cmd -P $PORT"
+  fi
+  if [ $2 ]; then
+    echo $1 |grep -q '/'
+    if [ $? -eq 0 ]; then
+      cmd="$cmd $HOST:$1 $2"
+    else
+      cmd="$cmd $HOST:~/$1 $2"
+    fi
+  else
+    cmd="$cmd $1 $HOST:~"
+  fi
+else
+  cmd="ssh"
+  if [ $PRI ]; then
+    cmd="$cmd -i $PRI"
+  fi
+  if [ $PORT ]; then
+    cmd="$cmd -p $PORT"
+  fi
+  cmd="$cmd $HOST"
+fi
+
+echo "--> $cmd"
+sh -c "$cmd"
