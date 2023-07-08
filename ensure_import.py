0aa2b42f64c6e18983a579321e274ef3ef8f69a5
Waket Zheng <waketzheng@gmail.com>
2023-07-08 11:14:04 +0800

    compare with poetry project

--- ensure_import.py
+++ ensure_import.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 from contextlib import AbstractContextManager
 from pathlib import Path
+from typing import Optional, Union
 
 
 class EnsureImport(AbstractContextManager):
@@ -25,15 +26,17 @@ class EnsureImport(AbstractContextManager):
 
     mapping = {
         "multipart": "python-multipart",
+        "tortoise": "tortoise-orm",
         "dotenv": "python-dotenv",
         "snap7": "python-snap7",
     }
-    retry = 10
+    retry = 30
 
-    def __init__(self, **kwargs):
-        self.exception = None
+    def __init__(self, _path: Optional[Path] = None, _exit=True, **kwargs):
         self._success = True
-        self.package_mapping = dict(self.mapping, **kwargs)
+        self._exit = _exit
+        self._path = _path
+        self._mapping = kwargs
 
     @property
     def ok(self) -> bool:
@@ -41,19 +44,18 @@ class EnsureImport(AbstractContextManager):
 
     def __exit__(self, exc_type, exc_value, traceback):
         if isinstance(exc_value, (ImportError, ModuleNotFoundError)):
-            self.exception = exc_value
             self._success = False
-            self.run()
+            self.run(exc_value)
             return True
 
-    def run(self):
-        e = self.exception
-        modules = re.findall(r"'([a-zA-Z][0-9a-zA-Z_-]+)'", str(e))
-        if "--no-install" in sys.argv or not modules:
+    def run(self, e):
+        modules = re.findall(r"'([a-zA-Z][0-9a-zA-Z_]+)'", str(e))
+        if not modules or "--no-install" in sys.argv:
             raise e
-        ms = (self.package_mapping.get(i, i) for i in modules)
+        package_mapping = dict(self.mapping, **self._mapping)
+        ms = (package_mapping.get(i, i) for i in modules)
         rc = self.install_and_extend_sys_path(*ms)
-        if rc:
+        if rc and self._exit:
             sys.exit(rc)
 
     @staticmethod
@@ -72,24 +74,49 @@ class EnsureImport(AbstractContextManager):
     def log_error(action: str) -> None:
         print(f"ERROR: failed to {action}")
 
-    @classmethod
-    def install_and_extend_sys_path(cls, *packages) -> int:
-        py = Path(sys.executable)
+    @staticmethod
+    def is_poetry_project(dirpath: Path) -> bool:
+        toml_name = "pyproject.toml"
+        for _ in range(3):
+            if dirpath.joinpath(toml_name).exists():
+                break
+            dirpath = dirpath.parent
+        else:
+            return False
+        cmd = "poetry run python -m pip --version"
+        return subprocess.run(cmd.split()).returncode == 0
+
+    @staticmethod
+    def get_poetry_py_path() -> Path:
+        cmd = "poetry env info --path"
+        r = subprocess.run(cmd.split(), capture_output=True)
+        return Path(r.stdout.strip().decode())
+
+    def install_and_extend_sys_path(self, *packages) -> int:
+        py: Union[str, Path] = Path(sys.executable)
         depends = " ".join(packages)
-        if not cls.is_venv():
-            p = Path.cwd() / "venv"
-            if not p.exists():
-                if cls.run_and_echo(f"{py} -m venv venv"):
-                    cls.log_error(f"create virtual environment for {py}")
-                    return 1
-            if platform.platform().lower().startswith("win"):
-                py = p / "Scripts" / "python.exe"
+        if not self.is_venv():
+            if self._path is None:
+                self._path = Path.cwd()
+            elif self._path.is_file():
+                self._path = self._path.parent
+            if self.is_poetry_project(self._path):
+                p = self.get_poetry_py_path()
+                py = "poetry run python"
             else:
-                py = p / "bin/python"
-            cls.run_and_echo(f"{py} -m pip install --upgrade pip")
+                p = self._path / "venv"
+                if not p.exists():
+                    if self.run_and_echo(f"{py} -m venv venv"):
+                        self.log_error(f"create virtual environment for {py}")
+                        return 1
+                if platform.platform().lower().startswith("win"):
+                    py = p / "Scripts" / "python.exe"
+                else:
+                    py = p / "bin/python"
+            self.run_and_echo(f"{py} -m pip install --upgrade pip")
             lib = list(p.rglob("site-packages"))[0]
             sys.path.append(lib.as_posix())
-        if cls.run_and_echo(f"{py} -m pip install {depends}"):
-            cls.log_error(f"install {depends}")
+        if self.run_and_echo(f"{py} -m pip install {depends}"):
+            self.log_error(f"install {depends}")
             return 2
         return 0
