14b62d5e1c58219713e442cb2429821cc28614e1
Waket Zheng <waketzheng@gmail.com>
2023-09-04 22:08:25 +0800

    Support pipi packages and add fast-tort-cli

--- .pipi.py
+++ .pipi.py
@@ -2,6 +2,7 @@
 import subprocess
 import sys
 from pathlib import Path
+
 """
 Custom install shortcut as i for pip
 
@@ -25,7 +26,7 @@ def main():
     p = Path(sys.executable).parent / "pip"
     if not p.exists():
         p = Path(capture_output("which pip"))
-    packages = " ".join(sys.argv[1:])
+    packages = " ".join(args := sys.argv[1:])
     if "install" not in (c := p.read_text()):
         ss = c.strip().splitlines()
         try:
@@ -33,8 +34,15 @@ def main():
         except PermissionError:
             print(f"Failed to write lines to {p}:\n{TEXT}")
             return 1
-        print("i command was configured!\n")
-    print(f"Use this instead:\n\n  pip i {packages}\n")
+        print("i command was configured!\n\nUsage::\n\n    pip i package-name\n")
+    elif not args:
+        print("--> pip install")
+        subprocess.run(["pip", "install"])
+    if args:
+        cmd = "pip i " + " ".join(args)
+        print("-->", cmd)
+        subprocess.run(cmd, shell=True)
+        print(f"Use this instead:\n\n  pip i {packages}\n")
 
 
 if __name__ == "__main__":
