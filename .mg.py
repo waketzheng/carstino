73cf249573bf06c1160a01cce3d3f999fcb47cda
Waket Zheng <waketzheng@gmail.com>
2023-09-16 10:20:20 +0800

    Use ipython module when manage.py not found

--- .mg.py
+++ .mg.py
@@ -10,10 +10,14 @@ def main():
             break
         fn = "../" + fn
     else:
+        if sys.argv[1:] and sys.argv[1] == "shell":
+            cmd = "python -m IPython"
+            print("--> {}".format(cmd))
+            return os.system(cmd)
         print("Error:\n    `manage.py` not found. Is it a django project?")
-        return
-    os.system("python {} {}".format(fn, " ".join(sys.argv[1:])))
+        return 1
+    return os.system("python {} {}".format(fn, " ".join(sys.argv[1:])))
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
