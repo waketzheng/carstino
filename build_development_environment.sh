1c6940da1c44bae321c174d094f6a37ba6ab93fd
Waket Zheng <waketzheng@gmail.com>
2024-03-25 18:05:33 +0800

    Script to install oh my zsh

--- build_development_environment.sh
+++ build_development_environment.sh
@@ -60,34 +60,10 @@ contains() {
 contains $* "--upgrade-py" && echo "---- Optional: install python3.11" && ./upgrade_py.py && python3.11 -m pip install --user -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple && python3.11 -m ensurepip && export PATH="$HOME/.local/bin:/usr/local/bin:$PATH"
 
 echo "---- Optional: install zsh"
-$SUDO apt install -y zsh
-if [ -f /etc/pam.d/chsh ]; then
-  # Make `chsh` no need to input password
-  # $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn,'a+');s=fp.read();fp.truncate();fp.write(s.replace(b,a));fp.close()"
-  $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn);s=fp.read();fp.close();fp=open(fn,'w');fp.write(s.replace(a,b));fp.close()"
-fi
-chsh -s $(which zsh)
-sh -c 'echo "[ -s \$HOME/.bash_aliases ] && source \$HOME/.bash_aliases" >> $HOME/.zshrc'
-sh -c 'echo "[ -s \$HOME/.local/bin ] && export PATH=\$HOME/.local/bin:/usr/local/bin:\$PATH" >> $HOME/.zshrc'
-sh -c 'echo "export ZSH=\$HOME/.oh-my-zsh" >>  $HOME/.zshrc'
-sh -c 'echo "ZSH_THEME=random" >>  $HOME/.zshrc'
-sh -c 'echo "plugins=(git pip python poetry)" >>  $HOME/.zshrc'
-sh -c 'echo "[ -s \$ZSH/oh-my-zsh.sh ] && source \$ZSH/oh-my-zsh.sh" >>  $HOME/.zshrc'
-
-if [ -v OMZ_REPO ]; then
-  echo use $OMZ_REPO for oh-my-zsh installing
-else
-  if [ -v USE_GITHUB ]; then
-    OMZ_REPO="https://github.com/ohmyzsh/oh-my-zsh"
-  else
-    OMZ_REPO="https://gitee.com/mirrors/oh-my-zsh"
-  fi
-fi
-export REMOTE="$OMZ_REPO.git" && sh -c "$(curl -fsSL $OMZ_REPO/raw/master/tools/install.sh)" --keep-zshrc
-
+bash ohmyzsh.sh
 
 echo "---- Init python development environment."
-./init_my_dev.py
+python3 init_my_dev.py
 
 echo "If you want to configure frontend development evironment, do this:"
 echo "    $ ./frontend_conf.sh"
---
+++ ohmyzsh.sh
@@ -0,0 +1,31 @@
+if [ "$USER" = "root" ]; then
+  SUDO=""
+else
+  echo "This script need root permission."
+  sudo echo "Go ahead..."
+  SUDO="sudo"
+fi
+$SUDO apt install -y zsh
+if [ -f /etc/pam.d/chsh ]; then
+  # Make `chsh` no need to input password
+  # $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn,'a+');s=fp.read();fp.truncate();fp.write(s.replace(b,a));fp.close()"
+  $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn);s=fp.read();fp.close();fp=open(fn,'w');fp.write(s.replace(a,b));fp.close()"
+fi
+chsh -s $(which zsh)
+sh -c 'echo "[ -s \$HOME/.bash_aliases ] && source \$HOME/.bash_aliases" >> $HOME/.zshrc'
+sh -c 'echo "[ -s \$HOME/.local/bin ] && export PATH=\$HOME/.local/bin:/usr/local/bin:\$PATH" >> $HOME/.zshrc'
+sh -c 'echo "export ZSH=\$HOME/.oh-my-zsh" >>  $HOME/.zshrc'
+sh -c 'echo "ZSH_THEME=random" >>  $HOME/.zshrc'
+sh -c 'echo "plugins=(git pip python poetry)" >>  $HOME/.zshrc'
+sh -c 'echo "[ -s \$ZSH/oh-my-zsh.sh ] && source \$ZSH/oh-my-zsh.sh" >>  $HOME/.zshrc'
+
+if [[ -v OMZ_REPO ]]; then
+  echo use $OMZ_REPO for oh-my-zsh installing
+else
+  if [[ -v USE_GITEE ]]; then
+    OMZ_REPO="https://gitee.com/mirrors/oh-my-zsh"
+  else
+    OMZ_REPO="https://github.com/ohmyzsh/oh-my-zsh"
+  fi
+fi
+export REMOTE="$OMZ_REPO.git" && sh -c "$(curl -fsSL $OMZ_REPO/raw/master/tools/install.sh)" --keep-zshrc
