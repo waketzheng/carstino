6acdcf452186b4129e2875ea2e477863478d2a15
Waket Zheng <waketzheng@gmail.com>
2024-10-16 11:37:09 +0800

    Update ohmyzsh script for centos

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-"""Use this script to install zsh and change default shell to it, then install ohmyzsh"""
+echo "Use this script to install zsh and change default shell to it, then install ohmyzsh"
 if [ "$USER" = "root" ]; then
   SUDO=""
 else
@@ -7,27 +7,59 @@ else
   sudo echo "Go ahead..."
   SUDO="sudo"
 fi
-$SUDO apt install -y zsh
+
+if [ -x "$(command -v apt)" ]; then
+  $SUDO apt install -y zsh
+elif [ -x "$(command -v yum)" ]; then
+  $SUDO yum install -y zsh
+elif [ -x "$(command -v brew)" ]; then
+  brew install zsh
+else
+  echo You may need to install zsh manually.
+fi
+
 if [ -f /etc/pam.d/chsh ]; then
   # Make `chsh` no need to input password
   # $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn,'a+');s=fp.read();fp.truncate();fp.write(s.replace(b,a));fp.close()"
   $SUDO python -c "fn='/etc/pam.d/chsh';a,b='required','sufficient';fp=open(fn);s=fp.read();fp.close();fp=open(fn,'w');fp.write(s.replace(a,b));fp.close()"
 fi
-chsh -s $(which zsh)
-sh -c 'echo "[ -s \$HOME/.bash_aliases ] && source \$HOME/.bash_aliases" >> $HOME/.zshrc'
-sh -c 'echo "[ -s \$HOME/.local/bin ] && export PATH=\$HOME/.local/bin:/usr/local/bin:\$PATH" >> $HOME/.zshrc'
-sh -c 'echo "export ZSH=\$HOME/.oh-my-zsh" >>  $HOME/.zshrc'
-sh -c 'echo "ZSH_THEME=random" >>  $HOME/.zshrc'
-sh -c 'echo "plugins=(git pip python poetry)" >>  $HOME/.zshrc'
-sh -c 'echo "[ -s \$ZSH/oh-my-zsh.sh ] && source \$ZSH/oh-my-zsh.sh" >>  $HOME/.zshrc'
-
-if [[ -v OMZ_REPO ]]; then
-  echo use $OMZ_REPO for oh-my-zsh installing
+if [ -f /bin/zsh ]; then
+  ZSH_EXEC=/bin/zsh
 else
-  if [[ -v USE_GITEE ]]; then
-    OMZ_REPO="https://gitee.com/mirrors/oh-my-zsh"
-  else
-    OMZ_REPO="https://github.com/ohmyzsh/oh-my-zsh"
-  fi
+  ZSH_EXEC=$(which zsh)
 fi
-export REMOTE="$OMZ_REPO.git" && sh -c "$(curl -fsSL $OMZ_REPO/raw/master/tools/install.sh)" --keep-zshrc
+chsh -s $ZSH_EXEC
+
+# Uncomment the following lines to expand PATH and plugins
+#sh -c 'echo "[ -s \$HOME/.bash_aliases ] && source \$HOME/.bash_aliases" >> $HOME/.zshrc'
+#sh -c 'echo "[ -s \$HOME/.local/bin ] && export PATH=\$HOME/.local/bin:/usr/local/bin:\$PATH" >> $HOME/.zshrc'
+#sh -c 'echo "export ZSH=\$HOME/.oh-my-zsh" >>  $HOME/.zshrc'
+#sh -c 'echo "ZSH_THEME=random" >>  $HOME/.zshrc'
+#sh -c 'echo "plugins=(git pip python poetry)" >>  $HOME/.zshrc'
+#sh -c 'echo "[ -s \$ZSH/oh-my-zsh.sh ] && source \$ZSH/oh-my-zsh.sh" >>  $HOME/.zshrc'
+
+if [[ -v REMOTE ]]; then
+    echo REMOTE value "$REMOTE" will be used to install ohmyzsh
+else
+    if [[ -v OMZ_REPO ]]; then
+      echo use $OMZ_REPO for oh-my-zsh installing
+    else
+      if [[ -v USE_GITEE ]]; then
+        OMZ_REPO="https://gitee.com/mirrors/oh-my-zsh"
+      else
+        OMZ_REPO="https://github.com/ohmyzsh/oh-my-zsh"
+      fi
+    fi
+    if [[ $OMZ_REPO == *.git ]]; then
+        export REMOTE="$OMZ_REPO"
+    else
+        export REMOTE="$OMZ_REPO.git"
+    fi
+fi
+
+if [[ $REMOTE == http* ]]; then
+  REMOTE=$REMOTE sh -c "$(curl -fsSL $OMZ_REPO/raw/master/tools/install.sh)" --keep-zshrc
+elif [[ $REMOTE == ssh* ]]; then
+  [ -d ohmyzsh ] || git clone $REMOTE
+  REMOTE=$REMOTE ./ohmyzsh/tools/install.sh
+fi
