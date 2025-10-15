5e4b0c50b6df77a79c08d14525be68ee383d512d
Waket Zheng <waketzheng@gmail.com>
2025-10-15 11:08:10 +0800

    fix: ssh script error at windows

--- to_my_server_43.139.125.122.sh
+++ to_my_server_43.139.125.122.sh
@@ -7,6 +7,10 @@
 
 FILE=$0  # filename of this script (xxx_<ip>.sh)
 STEM=$(basename $FILE .sh)  # parse file stem (xxx_<ip>)
-IP_OR_DOMAIN=$(echo $STEM | rev | cut -d '_' -f 1 | rev)  # split by '_' and get the last element (<ip>)
+if [ -f /usr/bin/rev ]; then
+    IP_OR_DOMAIN=$(echo $STEM | rev | cut -d '_' -f 1 | rev)  # split by '_' and get the last element (<ip>)
+else
+    IP_OR_DOMAIN=$(STEM=$STEM python -c "import os;stem=os.getenv('STEM');ip=stem.split('_')[-1];print(ip,end='')")
+fi
 
 SSH_USER=$SSH_USER SSH_PASS=$passwd DEST=$IP_OR_DOMAIN PRI=$PRI PORT=$PORT sh ~/archives/carstino/.from_here_to_there.sh $1 $2
