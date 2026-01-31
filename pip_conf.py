e4223c1bdea1398d0838023ce68a13bd22435335
Waket Zheng <waketzheng@gmail.com>
2026-01-31 12:22:08 +0800

    refactor: refresh updated at

--- pip_conf.py
+++ pip_conf.py
@@ -30,7 +30,7 @@ If there is any bug or feature request, report it to:
 """
 
 __author__ = "waketzheng@gmail.com"
-__updated_at__ = "2025.11.26"
+__updated_at__ = "2026.01.31"
 __version__ = "0.8.9"
 import contextlib
 import functools
